e98c718340ee8ba1eff7f23667325a5a6420a6f6
valeryndph-cloud <valeryndph@gmail.com>
2026-09-03 13:45:23 -0500

    feat: agregar menu de opciones a la Calculadora

--- calculadora.py
+++ calculadora.py
@@ -14,3 +14,34 @@ def dividir(a, b):
         raise ValueError("No se puede dividir entre cero.")
     return a / b
 
+print("Bienvenido a la calculadora")
+# vamos hacer un menu para que el usuario pueda elegir la operación que desea realizar
+numero1 = float(input("Ingrese el primer número: "))
+numero2 = float(input("Ingrese el segundo número: "))
+
+print ("####### Opciones #######") 
+print ("1. Sumar")
+print ("2. Restar")
+print ("3. Multiplicar")
+print ("4. Dividir")
+print ("########################")
+
+operacion = input("Seleccione la operación que desea realizar (1/2/3/4): ")
+
+if operacion == "1":
+    resultado = sumar(numero1, numero2)
+    print(f"El resultado de la suma es: {resultado}")
+elif operacion == "2":
+    resultado = restar(numero1, numero2)
+    print(f"El resultado de la resta es: {resultado}")
+elif operacion == "3":
+    resultado = multiplicar(numero1, numero2)
+    print(f"El resultado de la multiplicación es: {resultado}")
+elif operacion == "4":
+    try:
+        resultado = dividir(numero1, numero2)
+        print(f"El resultado de la división es: {resultado}")
+    except ValueError as e:
+        print(f"Error: {e}")
+else:
+    print("Operación no válida")
